f8cd8357cd2614eb28ff3676b8dcd1ea2683b311
smmazharul <hmoxygen@gmail.com>
2025-11-19 19:18:38 +0600

    calculate the rectangle area

--- input.py
+++ input.py
@@ -5,3 +5,12 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
 print(f"Hello, {name}! You are {age} years old.")
+
+
+# Exercise 1 Rectangle Area Calculator
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+
+print(f"The area of the rectangle is: {area}")
