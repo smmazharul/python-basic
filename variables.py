b28767c2deb9a90be52743166d36e7155f457ec8
smmazharul <hmoxygen@gmail.com>
2025-11-19 18:29:06 +0600

    learn python 4 variable string, integer,float and boolean

---
+++ variables.py
@@ -0,0 +1,36 @@
+# Variable = A container for a value (string , integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# string variable
+first_name = "john"
+food = "pizza"
+email = "jhon@gmail.com"
+print (f"first_name: {first_name}")
+print(f"You like {food}")
+print(f"Your email is: {email}")
+
+
+# integer variable
+age = 25
+quantity = 10
+num_of_students = 30
+print(f"you are {age} years old")
+print (f"you have {quantity} items")
+print (f"There are {num_of_students} students in the class")
+
+
+# float variable
+price = 19.99
+gpa = 3.5
+distance = 12.7
+print(f"The price is ${price}")
+print (f"Your GPA is {gpa}")
+print (f"The distance is {distance} miles")
+
+# boolean variable
+is_student = False
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
