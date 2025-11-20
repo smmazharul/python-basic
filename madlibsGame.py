fdfcfc691e0889434aa49856e597f39b4312d72a
smmazharul <hmoxygen@gmail.com>
2025-11-20 18:12:01 +0600

    madlibs game

---
+++ madlibsGame.py
@@ -0,0 +1,12 @@
+#Madlibs Game 
+
+
+
+print("Welcome to the Madlibs Game!")
+
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+adjective1 = input("Enter an adjective: ")
+adverb1 = input("Enter an adverb: ")
+
+print(f"Once upon a time, there was a {adjective1} {noun1} who loved to {verb1} {adverb1}.")
